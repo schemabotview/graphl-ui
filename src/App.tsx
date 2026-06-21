e15453141749b6cd6584e18889e991e7fc7e47fe
maddipotiganesh <maddipoti.ganesh@gmail.com>
2026-06-21 21:55:57 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { PresentationView } from './pages/PresentationView'
 function App() {
   return (
     <BrowserRouter>
-      <div className="h-full bg-[#0a0a0a] text-slate-100">
+      <div className="h-full bg-[#252525] text-slate-100">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/p/:presentationId" element={<Navigate to="0" replace />} />
